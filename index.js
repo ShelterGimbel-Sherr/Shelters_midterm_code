82cc514efee1bd4962deb46a5fc64887dcab6199
ShelterGimbel-Sherr <59702539+ShelterGimbel-Sherr@users.noreply.github.com>
2020-01-22 21:41:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -22,62 +22,24 @@ function onChange(evt) {
     $(this)
       .removeClass("correct")
       .addClass("incorrect");
-    if (Math.random() > 0.1) {
+    if (Math.random() > .1) {
       sad.play();
     }
   }
 }
-window.onload = function() {
-    var wrapper = document.getElementById('wrapper');
-    var wfInterval = setInterval(flowers, 1300);
-    var counterTwo = 0;
-    
-    function flowers() {
-        var posTwo = Math.floor(Math.random() * 100) + 1;
-        var whiteflower = document.createElement('div');
-        whiteflower.setAttribute('class', 'flowers');
-        whiteflower.classList.add('whiteflower');
-        
-        var wfPetalOne = document.createElement('div');
-        wfPetalOne.setAttribute('class', 'wf-petal-one');
-        var wfPetalTwo = document.createElement('div');
-        wfPetalTwo.setAttribute('class', 'wf-petal-two');
-        var wfPetalThree = document.createElement('div');
-        wfPetalThree.setAttribute('class', 'wf-petal-three');
-        
-        whiteflower.appendChild(wfPetalOne);
-        whiteflower.appendChild(wfPetalTwo);
-        whiteflower.appendChild(wfPetalThree);
-        
-        whiteflower.style.left = posTwo+'%';
-        wrapper.appendChild(whiteflower);
-        
-        counter = counterTwo++;
-        
-        setTimeout(function(){
-            var posOne = Math.floor(Math.random() * 100) + 1;
-            var sunflower = document.createElement('div');
-            sunflower.setAttribute('class', 'flowers');
-            sunflower.classList.add('sunflower');
-        
-            var sfPetalOne = document.createElement('div');
-            sfPetalOne.setAttribute('class', 'sf-petal-one');
-            var sfPetalTwo = document.createElement('div');
-            sfPetalTwo.setAttribute('class', 'sf-petal-two');
-            var sfPetalThree = document.createElement('div');
-            sfPetalThree.setAttribute('class', 'sf-petal-three');
-        
-            sunflower.appendChild(sfPetalOne);
-            sunflower.appendChild(sfPetalTwo);
-            sunflower.appendChild(sfPetalThree);
-        
-        
-            sunflower.style.left = posOne+'%';
-            wrapper.appendChild(sunflower);
-        }, 1500);
-        
-        if(counter == 5) {
-            clearInterval(wfInterval);
-        }
-    }
+console.clear();
+$("input").change(onChange);
+function onChange(evt) {
+  let correct = $(this).data("correct");
+  let response = $(this).val();
+  console.log(correct, response);
+  if (correct == response) {
+    $(this)
+      .removeClass("incorrect")
+      .addClass("correct");
+  } else {
+    $(this)
+      .removeClass("correct")
+      .addClass("incorrect");
+  }
 }
